395a787a485abb8ca692fe65cf0a40d4f5ce328f
guifarinela <guilherme_farinela@grendene.com.br>
2024-05-22 15:22:36 -0300

    Feito

--- obj/Debug/net8.0/SorteadorDeTime.AssemblyInfo.cs
+++ obj/Debug/net8.0/SorteadorDeTime.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SorteadorDeTime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05d2b0ebcab4ce4993e0c3cfbaf5338c966394bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1260f7fc8ae06bf3a925fb4553c6f63cfa27d1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SorteadorDeTime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SorteadorDeTime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
